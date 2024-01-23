d5561cee24ca837f277a0cb5e6818869d93ec77e
chandhu1403 <139038029+chandhu1403@users.noreply.github.com>
2024-01-23 21:21:18 +0530

    Add files via upload

--- cy.py
+++ cy.py
@@ -1,12 +1,11 @@
-from flask import Flask
-a=Flask(__name__)
-@a.route("/")
-def fun1():
-  return"<h1><a href = https://iittp.codetantra.com></a></h1>"
-@a.route("/one")
-def fun2():
-  return"<h1>bits csw</h1>"
-@a.route("/two")
-def fun3():
-  return"<h1>bits cse</h1>"
-a.run()
+import mysql.connector
+myconn = mysql.connector.connect(host="localhost" ,user="root",passwd="",database="SampleDB")
+cur = myconn.cursor()
+cur.execute("select * from students")
+result = cur.fetchall()
+print("Student Details are :")
+for x in result:
+  print(x);
+myconn.commit()
+myconn.close()
+ 
